9ea9bd168e8148381bfad565ff7ee54de2e03cbd
Cristian Ogliari <cogliari@gmail.com>
2021-01-23 15:26:07 -0300

    first commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,9 @@
+import * from '../../assets/css/App.css';
+
+function HomePage() {
+  return (
+    <h1>Hello World</h1>
+  );
+}
+
+export default HomePage;
---
+++ src/utils/constants.js
@@ -0,0 +1,3 @@
+const BASE_API = 'http://localhost:3002/';
+
+export BASE_API;
